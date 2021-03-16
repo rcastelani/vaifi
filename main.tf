5df363dd34ffe769a2b1562b8d5eeb193ae76934
rcastelani <80698445+rcastelani@users.noreply.github.com>
2021-03-16 16:33:30 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,21 @@ provider "aws" {
   region  = "sa-east-1"
 }
 
+resource "tls_private_key" "this" {
+  algorithm = "RSA"
+}
+
+module "key_pair" {
+  source = "terraform-aws-modules/key-pair/aws"
+
+  key_name   = "aws-tf"
+  public_key = tls_private_key.this.public_key_openssh
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-01eb71e14cef400e2"
   instance_type = "t2.micro"
+  key_name = "aws-tf"
 
   tags = {
     Name = "ExampleInstance"
